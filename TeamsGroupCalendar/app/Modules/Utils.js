2e477d514a1678df2f48e130f352c3e10034a835
Dave Amato <daveamato@live.com>
2020-06-23 15:35:51 -0400

    updates

--- TeamsGroupCalendar/app/Modules/Utils.js
+++ TeamsGroupCalendar/app/Modules/Utils.js
@@ -1,3 +1,14 @@
+const getConfig = () => {
+   var config = {
+        clientId : "7fc9230c-fcd2-47fb-a2a3-6fb9a5ecda62",
+        redirectUri : "/TeamsGroupCalendar/app/silent-end.html",
+        authwindow :  "/TeamsGroupCalendar/app/auth.html",
+        content: "/TeamGroupCalendar/app/tab.html",
+        hostRoot: "https://atlasoutdoor.github.io",
+   };
+   return config;
+}
+
 const Authuser = (upn,appconfig) => {
     return new Promise(
         (resolve, reject) => {
